f0f281f704681cd53f8c76951da390c7a2f3ffb2
Ivan Sandrini <ivan.sandrini@contaazul.com>
2018-07-03 21:38:40 -0300

    example list queues

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
+)
+
+func main() {
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1")},
+	)
+	svc := sqs.New(sess)
+
+	result, err := svc.ListQueues(nil)
+	if err != nil {
+		fmt.Println("Error ", err)
+	}
+
+	fmt.Println("Success")
+	for i, urls := range result.QueueUrls {
+		if urls == nil {
+			continue
+		}
+		fmt.Printf("%d: %s\n", i, *urls)
+	}
+}
